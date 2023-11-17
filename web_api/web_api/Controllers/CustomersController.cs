d11fc6b63cd4eac87fb61259242ac0ce7c53a677
DaMon-14 <DaMon-14@users.noreply.github.com>
2023-11-17 20:39:58 +0200

    Modify customer post

--- web_api/web_api/Controllers/CustomersController.cs
+++ web_api/web_api/Controllers/CustomersController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace web_api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/")]
     public class CustomersController : ControllerBase
     {
         private readonly MyContext _context;
@@ -15,7 +17,7 @@ namespace web_api.Controllers
 
 
         // GET: api/Customers/5
-        [HttpGet("{id}")]
+        [HttpGet("Customers/{id}")]
         public async Task<ActionResult<Customer>> GetCustomer(int id)
         {
             if (_context.Customers == null)
@@ -33,7 +35,7 @@ namespace web_api.Controllers
 
         // POST: api/Customers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
+        [HttpPost("Customers/")]
         public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
         {
             if (_context.Customers == null)
@@ -46,8 +48,39 @@ namespace web_api.Controllers
             return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
         }
 
+        // POST: api/ModifyCustomers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("ModifyCustomers/{id}")]
+        public async Task<ActionResult<Customer>> ModifyCustomer(Customer customer, int id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("nothing to modify");
+            }
+            Customer getcustomer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(getcustomer);
+            if (customer.UserName == "string")
+            {
+                customer.UserName = getcustomer.UserName;
+            }
+            if(customer.Password == "string")
+            {
+                customer.Password = getcustomer.Password;
+            }
+            if (customer.Points == 0)
+            {
+                customer.Points = getcustomer.Points;
+            }
+            customer.CustomerID = id;
+
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerID }, customer);
+        }
+
         // DELETE: api/Customers/5
-        [HttpDelete("{id}")]
+        [HttpDelete("Customers/{id}")]
         public async Task<IActionResult> DeleteCustomer(int id)
         {
             if (_context.Customers == null)
