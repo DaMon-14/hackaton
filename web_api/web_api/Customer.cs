4c66842ae6d62c988cb5fbe5c25a230f36c59019
DaMon-14 <DaMon-14@users.noreply.github.com>
2023-11-17 12:16:16 +0200

    intial commit

---
+++ web_api/web_api/Customer.cs
@@ -0,0 +1,18 @@
+namespace web_api
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        public Client ClientInfo { get; set; }
+
+        public int Points {  get; set; }
+    }
+
+
+    public class Client
+    {
+    public string UserName {  get; set; }
+    public string Password { get; set; }
+    }
+}
